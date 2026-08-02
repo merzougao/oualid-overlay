7c812d9825eead9d7d85b23d49a9b300552e106e
oualid <merzougao@gmail.com>
2026-08-01 23:56:31 -0400

    Corrected the sed command

--- app-misc/web-launch/files/web-launch.sh
+++ app-misc/web-launch/files/web-launch.sh
@@ -6,7 +6,7 @@ if [ -n "$1" ]; then
 	q=$(printf %s "$*" | jq -sRr @uri)
 	$BROWSER "https://www.google.com/search?q=$q" >/dev/null 2>&1 &
 else
-	result=$(dmenu < "$HOME"/.browser/bookmarks.txt | cut -d' ' -f1)
+	result=$(dmenu < "$HOME"/.browser/bookmarks.txt | sed "s|^[^ ]*\([^ ]*\)|\1|")
 	[ -n "$result" ] || exit 1
 	$BROWSER "$result"
 fi
